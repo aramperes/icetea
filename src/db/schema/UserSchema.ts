6df0902b554c913cd886c704a175a5b7b7014c22
momothereal <momothereal.mc@gmail.com>
2018-01-27 23:21:20 -0500

    Add non-null validator

--- src/db/schema/UserSchema.ts
+++ src/db/schema/UserSchema.ts
@@ -10,10 +10,16 @@ export default class UserSchema extends Schema {
     private password_hash: string = undefined;
 
     set_password(password: string): void {
+        if (!password) {
+            return;
+        }
         this.password_hash = bcrypt.hashSync(password, 10);
     }
 
     password_match(password: string): boolean {
+        if (!password) {
+            return false;
+        }
         return bcrypt.compareSync(password, this.password_hash);
     }
 
