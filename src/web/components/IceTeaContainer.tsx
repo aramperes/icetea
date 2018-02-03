de05b80c01b7870a7ec6e180519d52629df58e20
momothereal <momothereal.mc@gmail.com>
2018-02-02 20:02:12 -0500

    Search bar

--- src/web/components/IceTeaContainer.tsx
+++ src/web/components/IceTeaContainer.tsx
@@ -50,6 +50,21 @@ export default class IceTeaContainer extends React.Component<IceTeaContainerProp
                     <div className={"IceTeaContainer-navbar-icon"}/>
                 </a>
                 {domNav}
+                <div className={"IceTeaContainer-navbar-search-group"}>
+                    <input type="text" id="navbar-search" className={"IceTeaContainer-navbar-search"}
+                           placeholder={"Search"}/>
+                    <div className={"IceTeaContainer-navbar-searchresults-group"}>
+                        <a href={"/issues/ABC-123"} className={"IceTeaContainer-navbar-searchresults-item"}>
+                            [ABC-123] Please fix this bug
+                        </a>
+                        <a href={"/issues/ABC-123"} className={"IceTeaContainer-navbar-searchresults-item"}>
+                            [ABC-456] The page loads slowly!
+                        </a>
+                        <a href={"/issues/ABC-123"} className={"IceTeaContainer-navbar-searchresults-item"}>
+                            [ABC-789] ice tea is the best
+                        </a>
+                    </div>
+                </div>
             </div>
         )
     }
