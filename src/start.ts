4d1f5af3a199ef888b675e86303ed2374d9917c9
momothereal <momothereal.mc@gmail.com>
2018-02-05 22:51:07 -0500

    Promisify startup and start work on search engine

--- src/start.ts
+++ src/start.ts
@@ -49,20 +49,22 @@ export const config = new ConfigurationLoader().loadConfiguration();
 console.log("port: " + config.port);
 console.log("database: " + config.mongodb.host + ":" + config.mongodb.port + "/" + config.mongodb.database.name + " (auth:" + config.mongodb.database.auth + ")");
 
-console.log("Connecting to database...");
-
 export const mongo = new MongoConnector(config.mongodb);
-mongo.connect((err) => {
-    if (err) {
-        console.error('Failed to connect to MongoDB database: ' + err.message);
-        process.exit(1);
-        return;
-    }
-    console.log("Connected to database.");
-    clearExpiredSessions();
+
+let connectToMongo = () => new Promise(resolve => {
+    console.log("Connecting to database...");
+    mongo.connect((err) => {
+        if (err) {
+            console.error('Failed to connect to MongoDB database: ' + err.message);
+            process.exit(1);
+            return;
+        }
+        console.log("Connected to database.");
+        resolve();
+    });
 });
 
-function clearExpiredSessions() {
+let clearExpiredSessions = () => new Promise(resolve => {
     console.log("Clearing expired sessions from database...");
     UserSessionSchema.clearExpiredSessions((err, result) => {
         if (err) {
@@ -71,22 +73,44 @@ function clearExpiredSessions() {
         } else {
             console.log("Cleared " + result.deletedCount + " expired sessions.");
         }
-        server = startWebServer();
+        resolve();
     });
-}
+});
 
-function startWebServer() {
+export let userSearchIndex = undefined;
+
+let createSearchIndex = () => new Promise(resolve => {
+    let userDataGetter = (err, index) => {
+        if (err) {
+            throw err;
+        }
+        // todo: why are there duplicates?
+        mongo._client.collection('user').find({}).stream().pipe(index.feed({objectMode: true}));
+        userSearchIndex = index;
+    };
+    require('search-index')({
+        searchable: false,
+        fieldOptions: {
+            name: {
+                searchable: true
+            }
+        }
+    }, userDataGetter);
+    resolve();
+});
+
+let startWebServer = () => new Promise(resolve => {
     console.log("Starting server...");
-    return new WebServer(__dirname, config)
+    server = new WebServer(__dirname, config)
         .middleware(new DefaultMiddleware)
         .middleware(new AuthMiddleware)
         .static()
         .router(new ApiRouter)
         .router(new AuthRouter)
         .router(new IndexRouter)
-        .listen((server) => {
+        .listen((s) => {
             console.log("Open to connections.");
-            postInit();
+            resolve();
         }, (err) => {
             if (err.code === 'EADDRINUSE') {
                 console.error('Failed to start server, port ' + err.port + ' is already in use.');
@@ -96,16 +120,16 @@ function startWebServer() {
             }
             process.exit(1);
         });
-}
+});
 
-function checkDefaultAdmin(callback: () => any) {
+let checkDefaultAdmin = () => new Promise(resolve => {
     let admin_username = config.general.admin_username;
     if (!admin_username || admin_username.trim() === "") {
         console.warn("***" +
             "\n  Warning: Default administrator username is not set in the configuration." +
             "\n  This option is found at dist/config.json/general/admin_username." +
             "\n***");
-        callback();
+        resolve();
         return;
     }
     let user = new UserSchema();
@@ -120,38 +144,56 @@ function checkDefaultAdmin(callback: () => any) {
                 "\n  Creating an account with this name will make it an administrator automatically." +
                 "\n***");
         }
-        callback();
+        resolve();
     });
-}
+});
 
-function postInit() {
-    checkDefaultAdmin(() => {
-        console.log("Enter 'stop' or 'exit' to close the server.");
-        let rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout,
-            terminal: true
-        });
-        rl.on('line', (input) => {
-            if (!input) {
-                return;
-            }
-            input = input.trim();
-            if (input.toLowerCase() === "exit" || input.toLowerCase() === "stop") {
-                peacefulShutdown();
-                return;
-            }
-            if (input.toLowerCase() === "version" || input.toLowerCase() === "about") {
-                let moduleFile = path.join(__dirname, "../package.json");
-                fs.exists(moduleFile, (exists) => {
-                    if (exists === false) {
-                        console.error("Failed to retrieve module information.");
-                    } else {
-                        let moduleInfo = require(moduleFile);
-                        console.log("icetea version " + moduleInfo.version);
-                    }
-                });
-            }
-        });
+let postInit = () => new Promise(resolve => {
+    console.log("Enter 'stop' or 'exit' to close the server.");
+    let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: true
     });
-}
+    rl.on('line', (input) => {
+        if (!input) {
+            return;
+        }
+        input = input.trim();
+        if (input.toLowerCase() === "exit" || input.toLowerCase() === "stop") {
+            peacefulShutdown();
+            return;
+        }
+        if (input.toLowerCase() === "version" || input.toLowerCase() === "about") {
+            let moduleFile = path.join(__dirname, "../package.json");
+            fs.exists(moduleFile, (exists) => {
+                if (exists === false) {
+                    console.error("Failed to retrieve module information.");
+                } else {
+                    let moduleInfo = require(moduleFile);
+                    console.log("icetea version " + moduleInfo.version);
+                }
+            });
+        }
+    });
+    /* Example search
+    userSearchIndex.match({
+        beginsWith: 'dem', threshold: 2, type: 'ID'
+    }).on('data', (data) => {
+        userSearchIndex.get(data.documents).on('data', (doc) => {
+            console.log(doc);
+        });
+    }).on('end', () => {
+        console.log('search done');
+    }); */
+    resolve();
+});
+
+// start server
+Promise.resolve()
+    .then(connectToMongo)
+    .then(clearExpiredSessions)
+    .then(createSearchIndex)
+    .then(startWebServer)
+    .then(checkDefaultAdmin)
+    .then(postInit);
